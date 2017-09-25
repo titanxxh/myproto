ad2d3df66755602ad2dacda8dc778a7ce54da002
Xinhao Xu <titanxxh@gmail.com>
2017-09-25 23:24:04 +0800

    add benchmark

--- example/api/api_test.go
+++ example/api/api_test.go
@@ -23,3 +23,36 @@ func TestTypeMap_GetProducer(t *testing.T) {
 	fmt.Printf("%+v\n", protoMsg)
 	assert.Equal(t, origin, protoMsg)
 }
+
+func BenchmarkTypeMap_GetProducer_UseProducer(b *testing.B) {
+	p := GlobalTypeMap.GetProducer("api.ProtoBufA")
+	origin := &ProtoBufA{
+		Name:     "hello world",
+		BirthDay: 20170925,
+		Phone:    "secret",
+		Siblings: 1,
+		Spouse:   true,
+		Money:    0.37,
+	}
+	buf, _ := proto.Marshal(origin)
+	for i := 0; i < b.N; i++ {
+		protoMsg := p()
+		proto.Unmarshal(buf, protoMsg)
+	}
+}
+
+func BenchmarkTypeMap_GetProducer_UseStruct(b *testing.B) {
+	origin := &ProtoBufA{
+		Name:     "hello world",
+		BirthDay: 20170925,
+		Phone:    "secret",
+		Siblings: 1,
+		Spouse:   true,
+		Money:    0.37,
+	}
+	buf, _ := proto.Marshal(origin)
+	for i := 0; i < b.N; i++ {
+		protoMsg := &ProtoBufA{}
+		proto.Unmarshal(buf, protoMsg)
+	}
+}
